4cddf097555bef9fea15311b38b5bb85b456b2dc
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-25 14:46:54 +0100

    Add files via upload

---
+++ multiplication-table.js
@@ -0,0 +1,14 @@
+// Multiplication table 9
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " * " + i + " = " + result);
+}
+
+// Multiplication tables 1 till 10
+for (let multiplier = 0; multiplier <=10; multiplier++) {
+    for (let i = 0; i <= 10; i++) {
+        let result = multiplier * i;
+        console.log(multiplier + " * " + i + " = " + result);
+    }
+}
